3fe25997a14367cd949d56ce024a2bb1a8349591
sebastian thielke <seb@firstnoodle.io>
2020-08-12 16:35:53 +0200

    App.vue → applying different tailwind classes

--- src/App.vue
+++ src/App.vue
@@ -1,83 +1,91 @@
 <template>
-    <div id="app">
+    <div id="app" class="p-16">
         <h1>{{ date }}</h1>
         <renderless-calendar ref="calendarView" :value="date" @change="onDateChange" :options="options">
             <div
                 slot="MONTH"
                 slot-scope="{ calendar, changeView, moveViewDate, viewData, viewMonth }"
-                class="calendar-view__page"
+                class="block w-64 p-4 text-xs border border-gray-300 rounded shadow-md"
             >
-                <nav class="calendar-view__nav">
+                <nav class="w-56 grid grid-cols-7">
                     <nav-button icon="double-left" @click="moveViewDate(-1, 'YEAR')" />
                     <nav-button icon="single-left" @click="moveViewDate(-1, 'MONTH')" />
-                    <div class="calendar-view__cell span-3">
-                        <button @click="changeView('YEAR')">{{ viewMonth }}</button>
+                    <div class="col-span-3 flex justify-center">
+                        <button class="inline-flex items-center h-8 text-blue-600 mr-1" @click="changeView('YEAR')">{{ viewMonth }}</button>
                         <button
+                            class="inline-flex items-center h-8 text-blue-600"
                             @click="changeView('DECADE')"
-                        >{{ calendar.dateCursor.getUTCFullYear() }}</button>
+                        >
+                            {{ calendar.dateCursor.getUTCFullYear() }}
+                        </button>
                     </div>
-                    <nav-button icon="double-right" @click="moveViewDate(1, 'YEAR')" />
                     <nav-button icon="single-right" @click="moveViewDate(1, 'MONTH')" />
+                    <nav-button icon="double-right" @click="moveViewDate(1, 'YEAR')" />
                 </nav>
-                <header>
-                    <div class="calendar-view__cell" v-for="day in weekdays" :key="day">{{ day.slice(0, 3) }}</div>
+                <header class="w-56 grid grid-cols-7">
+                    <div class="flex items-center justify-center w-8 h-8 text-gray-800 select-none" v-for="day in weekdays" :key="day">{{ day.slice(0, 3) }}</div>
                 </header>
-                <main>
-                    <div class="calendar-view__cell" v-for="col in viewData" :key="col.date">
-                        <span
-                            @click="date = col.date"
-                            :class="{
-                                'is-selected': col.selected,
-                                'is-disabled': col.disabled,
-                                'is-inactive': col.inactive,
-                                'is-current': col.current,
-                            }"
-                        >{{ col.label }}</span>
-                    </div>
+                <main class="w-56 grid grid-cols-7">
+                    <button
+                        v-for="col in viewData" :key="col.date"
+                        @click="date = col.date"
+                        class="css-calendar-cell flex items-center justify-center w-8 h-8 rounded-full cursor-pointer text-gray-600 hover:bg-blue-100 focus:outline-none focus:bg-blue-100"
+                        :class="{
+                            'is-selected': col.selected,
+                            'is-disabled': col.disabled,
+                            'is-inactive': col.inactive,
+                            'is-current': col.current,
+                        }"
+                    >
+                        {{ col.label }}
+                    </button>
                 </main>
             </div>
 
             <div
                 slot="YEAR"
                 slot-scope="{ calendar, moveViewDate, setViewDateAndChangeView, viewData }"
-                class="calendar-view__page"
+                class="block w-64 p-4 text-xs border border-gray-300 rounded shadow-md"
             >
-                <nav>
+                <nav class="w-56 grid grid-cols-7">
                     <nav-button icon="double-left" @click="moveViewDate(-1, 'YEAR')" />
-                    <div class="calendar-view__cell span-5">{{ calendar.dateCursor.getUTCFullYear() }}</div>
+                    <div class="col-span-5 flex justify-center items-center">{{ calendar.dateCursor.getUTCFullYear() }}</div>
                     <nav-button icon="double-right" @click="moveViewDate(1, 'YEAR')" />
                 </nav>
-                <main>
-                    <div
+                <main class="w-56 grid grid-cols-2 py-4">
+                    <button
                         v-for="(month, monthIndex) in viewData"
                         :key="month.label"
                         @click="setViewDateAndChangeView(monthIndex, 'month', 'MONTH')"
-                        class="calendar-view__cell span-3-5"
+                        class="h-8 flex items-center justify-center cursor-pointer text-blue-600"
+                        :class="{ 'is-current': month.current }"
                     >
-                        <span :class="{ 'is-current': month.current }">{{ month.label }}</span>
-                    </div>
+                        {{ month.label }}
+                    </button>
                 </main>
             </div>
 
             <div
                 slot="DECADE"
                 slot-scope="{ moveViewDate, setViewDateAndChangeView, viewData }"
-                class="calendar-view__page"
+                class="block w-64 p-4 text-xs border border-gray-300 rounded shadow-md"
             >
-                <nav>
+                <nav class="w-56 grid grid-cols-7">
                     <nav-button icon="double-left" @click="moveViewDate(-10, 'YEAR')" />
-                    <div class="calendar-view__cell span-5">{{ viewData[0].label + ' - ' + viewData[viewData.length-1].label }}</div>
+                    <div class="col-span-5 flex items-center justify-center">{{ viewData[0].label + ' - ' + viewData[viewData.length-1].label }}</div>
                     <nav-button icon="double-right" @click="moveViewDate(10, 'YEAR')" />
                 </nav>
-                <main>
-                    <div
+                </main>
+                <main class="w-56 grid grid-cols-2 py-8">
+                    <button
                         v-for="year in viewData"
                         :key="year.label"
                         @click="setViewDateAndChangeView(year.label, 'year', 'MONTH')"
-                        class="calendar-view__cell span-3-5"
+                        class="h-8 flex items-center justify-center cursor-pointer text-blue-600"
+                        :class="{ 'is-current': year.current }"
                     >
-                        <span :class="{ 'is-current': year.current }">{{ year.label }}</span>
-                    </div>
+                        {{ year.label }}
+                    </button>
                 </main>
             </div>
         </renderless-calendar>
@@ -86,6 +94,7 @@
 
 <script>
 import { arrowKeys } from "./utils/keyboard.js";
+import { loopRange } from "./utils/array.js";
 import { disableWeekday, WEEKDAYS } from "./utils/time/calendar.js";
 import RenderlessCalendar from "./components/RenderlessCalendar/main.js";
 import NavButton from "./components/NavButton";
@@ -100,8 +109,18 @@ export default {
                 weekStart: 1,
                 disabledDate: disableWeekday("sunday")
             },
-            weekdays: WEEKDAYS
+            weekdays: null,
         };
+    },
+    created() {
+        this.weekdays = WEEKDAYS.map((day, index) => {
+            return WEEKDAYS[
+                loopRange(
+                    index + this.options.weekStart,
+                    WEEKDAYS.length
+                )
+            ];
+        })
     },
     mounted() {
         window.addEventListener("keydown", event => {
@@ -119,17 +138,3 @@ export default {
     }
 };
 </script>
-
-<style>
-@import url("https://fonts.googleapis.com/css?family=Roboto+Mono:400,700");
-
-* {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-}
-
-#app {
-    font-family: "Avenir", Helvetica, Arial, sans-serif;
-}
-</style>
